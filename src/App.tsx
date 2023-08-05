0dc1e5d433e9753fc41a24cba60a22ff347e5c7e
Yeison Hernandez <developer.ss.2@tucash.co>
2023-08-05 17:43:25 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Singleton } from "./creationalPatterns";
+
+function App() {
+  return (
+    <>
+      <h4>Creational patterns</h4>
+      <Singleton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/creationalPatterns/singleton/Singleton.tsx
@@ -0,0 +1,16 @@
+const Singleton = () => {
+  return (
+    <>
+      <h4>Singleton</h4>
+      <ul>
+        <li>Tipo de patron: Creacional</li>
+        <li>
+          Permite crear una instancia ÚNICA de un objeto en todo el sistema.
+        </li>
+        <li>Uso global de esta</li>
+      </ul>
+    </>
+  );
+};
+
+export default Singleton;
